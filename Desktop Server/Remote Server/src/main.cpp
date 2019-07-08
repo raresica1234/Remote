4bcddb40c7ffc8b37bb9e6822569b5b43c3720a6
raresica1234 <colorlord98@gmail.com>
2019-07-08 17:30:34 +0300

    Added console and attached std streams to it

--- Desktop Server/Remote Server/src/main.cpp
+++ Desktop Server/Remote Server/src/main.cpp
@@ -1,6 +1,42 @@
-#include "Windows.h"
-
+#include <Windows.h>
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+#ifdef DEBUG
+	AllocConsole();
+
+	// Get STDOUT handle
+	HANDLE ConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	int SystemOutput = _open_osfhandle(intptr_t(ConsoleOutput), _O_TEXT);
+	FILE *COutputHandle = _fdopen(SystemOutput, "w");
+
+	// Get STDERR handle
+	HANDLE ConsoleError = GetStdHandle(STD_ERROR_HANDLE);
+	int SystemError = _open_osfhandle(intptr_t(ConsoleError), _O_TEXT);
+	FILE *CErrorHandle = _fdopen(SystemError, "w");
+
+	// Get STDIN handle
+	HANDLE ConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
+	int SystemInput = _open_osfhandle(intptr_t(ConsoleInput), _O_TEXT);
+	FILE *CInputHandle = _fdopen(SystemInput, "r");
+
+	//make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog point to console as well
+	std::ios::sync_with_stdio(true);
+
+	// Redirect the CRT standard input, output, and error handles to the console
+	freopen_s(&CInputHandle, "CONIN$", "r", stdin);
+	freopen_s(&COutputHandle, "CONOUT$", "w", stdout);
+	freopen_s(&CErrorHandle, "CONOUT$", "w", stderr);
+
+#endif
+	std::cout.clear();
+
+
+	std::cout << "Test" << std::endl;
+	
+	std::cin.get();
+
 	return 0;
 }
